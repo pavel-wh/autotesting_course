1b23f4ed6470bca3cf02726cfed6bb97aff7a6c5
pavel <pavel.wh@yandex.ru>
2019-07-10 01:36:48 +0300

    update task 3

---
+++ settings.py
@@ -0,0 +1,3 @@
+BROWSER_DRIVER = "D:\\Soft\\Installed\\webDrivers\\chromedriver.exe"
+BROWSER_DRIVER_CHROME = "D:\\Soft\\Installed\\webDrivers\\chromedriver.exe"
+BROWSER_DRIVER_FIREFOX = r"D:\\Soft\\Installed\webDrivers\\geckodriver.exe"
--- test_items.py
+++ test_items.py
@@ -1,9 +1,9 @@
-import math
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_language(browser):
+def test_browser_language_basket_btn(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("button[type='submit']")
-    time.sleep(15)
+    time.sleep(5)
+    basketBtn = browser.find_element_by_css_selector(
+        "button.btn-add-to-basket[type='submit']")
