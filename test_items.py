e97377ae8f986a1412194114f337738edfd1b60e
pavel <pavel.wh@yandex.ru>
2019-07-10 01:38:07 +0300

    update task 3

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_browser_language_basket_btn(browser):
     browser.get(link)
-    time.sleep(5)
-    basketBtn = browser.find_element_by_css_selector(
+    time.sleep(30)
+    browser.find_element_by_css_selector(
         "button.btn-add-to-basket[type='submit']")
