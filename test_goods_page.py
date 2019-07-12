90818b4e83cacad5f2ac55ab5a645c7a293c42ac
pavel <pavel.wh@yandex.ru>
2019-07-12 11:27:51 +0300

    inheritance advantage

--- test_goods_page.py
+++ test_goods_page.py
@@ -19,7 +19,7 @@ import time
 #     print(link)
 
 
-def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
+def test_guest_cant_see_success_message_after_adding_goods_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = GoodsPage(browser, link)
@@ -38,7 +38,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
-def test_message_disappeared_after_adding_product_to_cart(browser):
+def test_message_disappeared_after_adding_goods_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = GoodsPage(browser, link)
@@ -47,3 +47,17 @@ def test_message_disappeared_after_adding_product_to_cart(browser):
     page.click_to_add_basket_button()
     time.sleep(2)  # Иначе не успевает появиться и тест падает сразу
     page.should_be_success_message_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = GoodsPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_goods_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = GoodsPage(browser, link)
+    page.open()
+    page.go_to_login_page()
