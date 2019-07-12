809799bb61bcd43f3ed923285ca08f4103a91c88
pavel <pavel.wh@yandex.ru>
2019-07-12 09:41:42 +0300

    data independence test search bug with parameters

--- test_goods_page.py
+++ test_goods_page.py
@@ -1,8 +1,10 @@
 from pages.goods_page import GoodsPage
+import pytest
 
 
-def test_guest_can_add_goods_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('offer', [i for i in range(0, 9)])
+def test_guest_can_add_goods_to_basket(browser, offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = GoodsPage(browser, link)
     page.open()  # открываем страницу
@@ -13,3 +15,4 @@ def test_guest_can_add_goods_to_basket(browser):
     # pytest -s -v --tb=line --language=en
     page.should_be_message_about_goods_name()
     page.should_be_message_about_goods_price()
+    print(link)
