7aa6cb14d613ecb4c94b5afbcd835267dff78623
pavel <pavel.wh@yandex.ru>
2019-07-12 07:39:13 +0300

    test go to login page from main page

--- test_main_page.py
+++ test_main_page.py
@@ -2,12 +2,14 @@ from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = MainPage(browser, link)
     page.open()                      # открываем страницу
     # выполняем метод страницы - переходим на страницу логина
     page.go_to_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
